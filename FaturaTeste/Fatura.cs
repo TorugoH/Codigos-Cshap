2991c61c44cc4e19fa91998b61d91dc648f00048
Vitor Hugo <vitorhugoemail@gmail.com>
2022-08-18 22:21:00 -0300

    primeiro codigo em POO

---
+++ FaturaTeste/Fatura.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaturaTeste
+{
+    class Fatura
+    {
+        private string _numero;
+        private string _descricao;
+        private int _qtIntem;
+        private double _precoItem;
+        public string getNumero() { return _numero; }
+        public void setNumero(string numero) { _numero = numero; }
+        public string getDescricao() { return _descricao; }
+        public void setDescricao(string descricao) { _descricao = descricao; }
+        public int getQTIntem() { return _qtIntem; }
+        public void setQTIntem(int qtIntem) { _qtIntem = qtIntem; }
+        public double getPrecoitem() { return _precoItem; }
+        public void setPrecoitem(double preco) { _precoItem= preco; }
+        public void getfatura()
+        {
+
+            if (_qtIntem * _precoItem >=0)
+            {
+                Console.WriteLine("Valor da sua fatura R$ "+_qtIntem * _precoItem);
+            }
+            else if (_qtIntem * _precoItem<=0)
+            {
+                Console.WriteLine("Valor da sua fatura R$ " + 0);
+            }
+        }
+    }
+}
---
+++ FaturaTeste/Program.cs
@@ -0,0 +1,20 @@
+﻿using FaturaTeste;
+using System;
+class FaturaTestes
+{
+    static void Main(string[] args){
+        Fatura fatura = new Fatura();
+        
+        Console.WriteLine("Digite a descricao do produto");
+        fatura.setDescricao(Console.ReadLine());
+        
+        Console.WriteLine("Digite o preço do produto");
+        fatura.setPrecoitem(double.Parse(Console.ReadLine()));
+        
+        Console.WriteLine("Digite a quantidade que deseja do produto");
+        fatura.setQTIntem(int.Parse(Console.ReadLine()));
+
+        fatura.getfatura();
+            
+    }
+}
