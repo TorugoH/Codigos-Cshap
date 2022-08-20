dd83a91a109d5e49e18f2224487a937d4d35af5e
Vitor Hugo <vitorhugoemail@gmail.com>
2022-08-20 18:55:20 -0300

    Codigo para estudar POO

---
+++ funcionarios/Funcionario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace funcionarios
+{
+    class Funcionario {
+        private string nome;
+        private string sobrenome;
+        private double salario;
+
+        public string getNome() { return nome; }
+        public void setNome(string Nome) { nome = Nome; }
+        public string getSobrenome() { return sobrenome; }
+        public void setSobrenome(string Sobrenome) { sobrenome = Sobrenome; }
+        public double getSalario() { return salario; }
+        public void setSalario(double Salario) { salario = Salario; }
+        public void dadosFuncionario(){
+            Console.WriteLine("Nome: "+nome);
+
+            Console.WriteLine("Sobrenome: "+sobrenome);
+            Console.WriteLine("Salario atual:" + salario);
+            Console.WriteLine("*********************************");
+        }
+        public void NovoSalario()
+        {
+            double novoSalario = salario + (salario * 0.10);
+            Console.WriteLine("Caro senhor(a)" + nome + " " + sobrenome);
+            Console.WriteLine("Seu novo salario e de " + novoSalario);
+            Console.WriteLine("*********************************");
+        }
+    }
+}
---
+++ funcionarios/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using funcionarios;
+class FuncionarioTeste
+{
+    static void Main(string[] args) {
+        Funcionario funcionario1 = new Funcionario();
+        Funcionario funcionario2 = new Funcionario();
+
+        Console.WriteLine("Digite o nome do funcionario");
+        funcionario1.setNome(Console.ReadLine());
+        Console.WriteLine("Digite o sobre nome do funcionario");
+        funcionario1.setSobrenome(Console.ReadLine());
+        Console.WriteLine("Digite o salario do funcionario");
+        funcionario1.setSalario(double.Parse(Console.ReadLine()));
+
+        Console.WriteLine("Digite os dados do segundo funcionario");
+
+        Console.WriteLine("Digite o nome do funcionario");
+        funcionario2.setNome(Console.ReadLine());
+        Console.WriteLine("Digite o sobre nome do funcionario");
+        funcionario2.setSobrenome(Console.ReadLine());
+        Console.WriteLine("Digite o salario do funcionario");
+        funcionario2.setSalario(double.Parse(Console.ReadLine()));
+
+        //Função resposavel por limpar o console
+        Console.Clear();
+
+        funcionario1.dadosFuncionario();
+        funcionario1.NovoSalario();
+        
+        funcionario2.dadosFuncionario();
+        funcionario2.NovoSalario();
+
+
+
+    }
+}
